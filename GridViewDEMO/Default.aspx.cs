002e5816fa955de08b857a10e138f6e6bd778639
Christos Kokkalis <uberck@gmail.com>
2019-06-21 00:13:03 -0400

    Modify database connection string

--- GridViewDEMO/Default.aspx.cs
+++ GridViewDEMO/Default.aspx.cs
@@ -42,8 +42,117 @@ namespace GridViewDEMO
             }
         }
 
-        //
+        // Database insert from "add" button
 
+        protected void Insert(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string country = txtCountry.Text;
+            txtName.Text = "";
+            txtCountry.Text = "";
+            string query = "INSERT INTO Customers VALUES(@Name, @Country)";
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Country", country);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindGrid();
+
+        }
+
+        // Editing records
+
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+
+        // updating database from GridView
+
+        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int customerId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string name = (row.FindControl("txtName") as TextBox).Text;
+            string country = (row.FindControl("txtCountry") as TextBox).Text;
+            string query = "UPDATE Customers SET Name=@Name, Country=@Country WHERE CustomerId=@CustomerId";
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerId", customerId);
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Country", country);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        // cancel edit
+
+        protected void OnRowCancelingEdit (object sender, EventArgs e)
+
+        {
+        
+        GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+
+        // database delete from GridView
+
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int customerId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string query = "DELETE FROM Customers WHERE CustomerId=@CustomerId";
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerId", customerId);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                
+            }
+
+            this.BindGrid();
+        }
+
+        // confirmation message
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridView1.EditIndex)
+            {
+                (e.Row.Cells[2].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
+            }
+        }
+
+        // paging
+
+        protected void OnPaging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            this.BindGrid();
+        }
 
     }
 }
